0471de6e55770b242d3f4b75f54ffb3ed0814ecb
Shafiya Heena <heena.shafiya@gmail.com>
2021-10-29 23:40:40 +0530

    midterm

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+ //"URI": "mongodb://localhost/business_contact"
+ "URI": "mongodb+srv://1234:1234@cluster0.1kc3i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+};
